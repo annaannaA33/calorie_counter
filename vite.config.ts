197502fd3734b5936aa88c873bed1600b249a765
annaannaA33 <annaansmva@gmail.com>
2024-12-09 00:16:45 +0200

    ci: add deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
